af0e5c954a41d06772472beae9bdeb7ca23309b0
MrPeachT <hextorj007@gmail.com>
2025-11-27 09:34:59 +0700

    Initialize commit

---
+++ src/components/WordForm.vue
@@ -0,0 +1,57 @@
+<template>
+    <form action="#" @submit.prevent="onSubmit">
+        <p v-if="errorsPresent" class="error">Please fill out all flieds!</p>
+        <div class="ui labeled input fluid">
+            <div class="ui label">
+                <i class="united kingdom flag"></i> English
+            </div>
+            <input type="text" placeholder="Enter word ..." v-model="words.english" />
+        </div>
+        <div class="ui labeled input fluid">
+            <div class="ui label">
+                <i class="germany flag"></i> German
+            </div>
+            <input type="text" placeholder="Enter word ..." v-model="words.german" />
+        </div>
+
+        <div class="ui labeled input fluid">
+            <div class="ui label">
+                <i class="vietnam flag"></i> Vietnamese
+            </div>
+            <input type="text" placeholder="Enter word ..." v-model="words.vietnamese" />
+        </div>
+    </form>
+</template>
+
+<script>
+export default {
+    name: 'word-form',
+    props: {
+        word: {
+            type: Object,
+            required: false,
+            default: () => {
+                return {
+                    english: '',
+                    german: '',
+                    vietnamese: ''
+                };
+            }
+        }
+    },
+    data() {
+        return {
+            errorsPresent: false,
+        };
+    },
+    methods: {
+        onSubmit: function() {
+            if (this.word.english === '' || this.word.german === '' || this.word.vietnamese === '') {
+                this.errorsPresent = true;
+            } else {
+                this.$emit('create0rUpdate', this.word);
+            }
+        }
+    }
+};
+</script>
---
+++ src/views/Show.vue
@@ -0,0 +1,15 @@
+<script>
+import { api } from '../helpers/helpers';
+export default {
+    name: 'show',
+    data() {
+        return {
+            word: '',
+        };
+    },
+    async mounted() {
+        this.word = await api.getWord(this.$route.params.id);
+    },
+};
+
+</script>
